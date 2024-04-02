e4eeda310a09a61c4e4cdf85da94c0faa8fe66d1
vikassukka <162142930+vikassukka@users.noreply.github.com>
2024-04-02 10:47:22 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -1,12 +1,12 @@
-def is_leap_year(year):
-    if (year%4==0 and year%100!=0):
-        return True
-    elif(year%100==0 and year%400==0):
-        return True
+year = int(input("Enter a year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print(year, "is a leap year.")
+        else:
+            print(year, "is not a leap year.")
     else:
-        return False
-year=int(input("Enter the year:"))
-if is_leap_year(year):
-    print(f"{year} is a leap year")
+        print(year, "is a leap year.")
 else:
-    print(f"{year} is not a leap year")
+    print(year, "is not a leap year.")
